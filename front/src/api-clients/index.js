df44c5baa93747e46a8a84158858facccff2df3e
gtyuki83 <yukiukiuki0803@gmail.com>
2022-07-03 02:10:37 +0900

    saving

--- front/src/api-clients/index.js
+++ front/src/api-clients/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const accessToken = "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";
+const accessToken = "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";
 
 export const fetchBranches = async () => {
   const response = await axios({
